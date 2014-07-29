f91ff8e2688156b2913a9b79e9ab8daaaa066999
bgaheer <baljeetgaheer@gmail.com>
2014-07-29 22:22:09 +0530

    new file

---
+++ SatinLibs/BusinessObjects/ProductCustomer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SatinLibs
+{
+   public class ProductCustomer
+    {
+       
+            #region Primitive Properties
+
+            public int Id
+            {
+                get;
+                set;
+            }
+
+            public int CustomerId
+            {
+                get;
+                set;
+            }
+            public string CustomerNo
+            {
+                get;
+                set;
+            }
+            public string ItemId
+            {
+                get;
+                set;
+            }
+            public string Ext_ItemId
+            {
+                get;
+                set;
+            }
+            public int Quantity
+            {
+                get;
+                set;
+            }
+
+            public int orderCuttOff
+            {
+                get;
+                set;
+            }
+            public int UOMultipler
+            {
+                get;
+                set;
+            }
+
+            #endregion
+        }
+    }
+
