98316393e77b89f4c8a2c813838e440358ffcaa0
bgaheer <baljeetgaheer@gmail.com>
2014-08-06 16:22:36 +0530

    fileutil

---
+++ SatinLibs/Utils/FileUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace SatinLibs
+{
+   public class FileUtils
+    {
+       public static String[] getFilesFromFolder(String folderPath, String extension)
+        {
+            String[] filePaths = Directory.GetFiles(@folderPath, "*." + extension);
+            return filePaths;
+        }
+    }
+}
