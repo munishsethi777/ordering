f8d5e5ac466b23fe5bab0b280bc5321a21c73615
bgaheer <baljeetgaheer@gmail.com>
2014-08-15 20:59:16 +0530

    orderutils

---
+++ SatinLibs/Utils/OrderUtil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MVCADOService;
+using MVCEF.Infrastructure;
+using MVCDataModel;
+
+namespace SatinLibs
+{
+    public class OrderUtil
+    {
+        public void inserOrder()
+        {
+            SatInHomeRepository repository = new SatInHomeRepository();
+            string orderNumber = repository.InsertOrder();
+            
+        }
+
+ 
+    }
+
+}
