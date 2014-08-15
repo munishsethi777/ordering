21b7c674546fa7af2212592a4b107a081166078f
bgaheer <baljeetgaheer@gmail.com>
2014-08-15 17:27:04 +0530

    inserOrder and jobManager

---
+++ SatinLibs/SchedulerManager/InsertOrder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Quartz.Job;
+using Quartz;
+using Quartz.Impl;
+
+namespace SatinLibs
+{
+
+    class InsertOrder: IJob
+  
+    {
+        private OrderUtil orderUtil = null;
+        public static string GetName()
+        {
+            return "InserOrderJob";
+        }
+
+
+        public static int GetRepetitionIntervalTime()
+        {
+           return 360;
+        }
+
+        public void Execute(IJobExecutionContext context)
+        {
+            if (orderUtil == null)
+            {
+                orderUtil = new OrderUtil();
+            }
+            orderUtil.inserOrder();
+        }
+    }
+}
---
+++ SatinLibs/SchedulerManager/JobManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Quartz;
+using Quartz.Impl;
+using Quartz.Job;
+
+namespace SatinLibs
+{
+    public class JobManager
+    {
+        public static void Runscheduler()
+        {
+            try
+            {
+                // Grab the Scheduler instance from the Factory 
+                IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+
+                // and start it off
+                scheduler.Start();
+
+
+                IJobDetail job = JobBuilder.Create<InsertOrder>()
+                   .WithIdentity(InsertOrder.GetName(), "group1")
+                   .Build();
+                //TimeSpan intervalTime = new TimeSpan(19,0,0);
+                // Trigger the job to run now, and then repeat every 10 seconds
+                ITrigger trigger = TriggerBuilder.Create()
+                    .WithIdentity("trigger1", "group1")
+                    .StartNow()
+                    .WithSimpleSchedule(x => x
+                        .WithIntervalInMinutes(30)
+                        .RepeatForever())
+                    .Build();
+
+                // Tell quartz to schedule the job using our trigger
+                scheduler.ScheduleJob(job, trigger);
+
+                // some sleep to show what's happening
+                //Thread.Sleep(TimeSpan.FromSeconds(5));
+                // and last shut down the scheduler when you are ready to close your program
+                //scheduler.Shutdown();
+            }
+            catch (SchedulerException se)
+            {
+                Console.WriteLine(se);
+            }
+        }
+    }
+}
