2b25a968c96fdbad4cafb47a11628553c03db84e
munishsethi777 <munishsethi777@gmail.com>
2014-08-21 07:54:12 +0530

    new files

---
+++ SatinLibs/Concrete/CustId708ParserExcel.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVCEF.Infrastructure;
+using System.Data;
+using System.Web;
+using System.Data.OleDb;
+
+namespace SatinLibs
+{
+   public class CustId2ParserExcel : ParserI
+    {
+        public DataSet getDataSet(string customerId, string fileLocation)
+        {
+            DataSet ds = new DataSet();
+            DataSet storesDataSet = CustomerUtils.getStores(customerId);
+            string excelConnectionString = string.Empty;
+            excelConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" +
+                    fileLocation + ";Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=1\"";
+            OleDbConnection excelConnection = new OleDbConnection(excelConnectionString);
+            excelConnection.Open();
+            DataTable dt = new DataTable();
+            dt = excelConnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+            if (dt == null)
+            {
+                return null;
+            }
+
+            String[] excelSheets = new String[dt.Rows.Count];
+            int t = 0;
+            //excel data saves in temp file here.
+            foreach (DataRow row in dt.Rows)
+            {
+                excelSheets[t] = row["TABLE_NAME"].ToString();
+                t++;
+            }
+            OleDbConnection excelConnection1 = new OleDbConnection(excelConnectionString);
+            string query = string.Format("Select * from [{0}]", excelSheets[0]);
+            using (OleDbDataAdapter dataAdapter = new OleDbDataAdapter(query, excelConnection1))
+            {
+                dataAdapter.Fill(ds);
+                excelConnection.Close();
+                excelConnection1.Close();
+            }
+
+            DataTable mytable = new DataTable();
+            DataRow totalItemRows = ds.Tables[0].Rows[6];
+            string totalRowsCountStr = (string)totalItemRows.ItemArray[1];
+            int totalRowsCount = int.Parse(totalRowsCountStr);
+            DataRow colHeaders = ds.Tables[0].Rows[9];
+            mytable.Columns.Add("Sl#");
+            mytable.Columns.Add("Product");
+            mytable.Columns.Add("Price");
+            int storesCount = storesDataSet.Tables[0].Rows.Count;
+
+            if (storesCount > 0)
+            {
+                for (int i = 0; i < storesCount; i++)
+                {
+                    mytable.Columns.Add(storesDataSet.Tables[0].Rows[i].ItemArray[0].ToString());
+                }
+            }
+            else
+            {
+                mytable.Columns.Add("Delivery");
+            }
+            mytable.Columns.Add("Remarks");
+
+            DataSet mydataset = new DataSet();
+
+            for (int i = 10; i <= totalRowsCount + 9; i++)
+            {
+                DataRow row = ds.Tables[0].Rows[i];
+                string itemNo = row.ItemArray[3].ToString();
+                string itemName = row.ItemArray[2].ToString();
+                string price = row.ItemArray[6].ToString();
+
+                object[] array = new object[storesCount + 4];
+                if (storesCount == 0)
+                {
+                    array = new object[1 + 4];
+                }
+
+
+                array[0] = itemNo;
+                array[1] = itemName;
+                array[2] = price;
+
+
+                int rowCounter = 3;
+                if (storesCount == 0)
+                {
+                    array[rowCounter++] = row.ItemArray[9].ToString();
+                }
+                else
+                {
+                    for (int j = 0; j < storesCount; j++)
+                    {
+                        array[rowCounter++] = row.ItemArray[9].ToString();
+                    }
+                }
+
+
+                string remarks = "";
+                array[rowCounter] = remarks;
+
+                mytable.Rows.Add(array);
+            }
+
+            mydataset.Tables.Add(mytable);
+            return mydataset;
+        }
+    }
+}
---
+++ SatinLibs/Utils/ValidatorUtil.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace SatinLibs
+{
+    public class ValidatorUtil
+    {
+        public static string[] validateUploadedFile(HttpPostedFileBase file, string customerId)
+        {
+            string[] errorMessage = new string[2] ;
+            string status = IConstants.FAILED;
+            string msg = IConstants.BLANK;
+            if (customerId.Equals(IConstants.BLANK) || customerId.Equals("0"))
+            {
+                msg = IConstants.SELECT_CUSTOMER;
+            }
+            else if (!IsFileTypeValid(file))
+            {
+                msg = IConstants.INVALID_FILE;
+            }
+            else if (!isStoreAvaiable(customerId))
+            {
+                msg = IConstants.NO_STORE;
+            }
+            else
+            {
+                status = IConstants.SUCCUSS;
+            }
+            errorMessage[0] = status;
+            errorMessage[1] = msg;
+            return errorMessage;
+        }
+
+
+        private static bool IsFileTypeValid(HttpPostedFileBase file)
+        {
+                bool isValid = false;
+                string ext = System.IO.Path.GetExtension(file.FileName);
+            try
+            {
+                if(ext.ToLower().Equals(".pdf"))
+                {
+                     isValid = true;
+                }
+
+                return isValid;
+            }
+           
+            catch 
+            {
+                return isValid;
+            }
+       }
+
+        public static bool isStoreAvaiable(string customerid)
+        {
+            DataSet dataSet = CustomerUtils.getStores(customerid);
+            return dataSet.Tables[0].Rows.Count > 0;
+        }
+
+        public static Dictionary<string, string> validateSaveOrders(DataTable sXMLOrders, string customerCode)
+        {
+            Dictionary<string, string> errorMap = new Dictionary<string, string>();
+            SatInHomeRepository objectRepository = new SatInHomeRepository();
+            Dictionary<String, ProductCustomer> map = objectRepository.getCustomerProductMap(customerCode);
+            if (map.Keys.Count == 0)
+            {
+                errorMap.Add("main_Error", "ProductCustomer Mapping does not exist for selected Customer");
+            }else{
+                int count = 1;
+                foreach (DataRow row in sXMLOrders.Rows)
+                {
+                    if (row[0] != null && row[0].ToString() != "0" && !string.IsNullOrEmpty(row[0].ToString()))
+                    {
+                        string ext_ItemId = row[1].ToString();
+                        string orderNumber = row[0].ToString();
+                        if (!map.Keys.Contains(ext_ItemId))
+                        {
+                            errorMap.Add(count + "." + "OrderNo." + orderNumber, "Error is SKU ID - " + ext_ItemId + " does not exist in ProductCustomer mapping");
+                            count++;
+                        }
+                    }
+                }
+            }
+            
+            return errorMap;
+        }
+    }
+}
